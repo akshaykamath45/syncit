3dd6a750aaea0a6109e89dc01d3ae6c3c7860d15
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-12-02 02:41:32 +0530

    fix: moving api key to .env

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWQDvaGoMtURmZSsPZAEeD9Ic84Xk6_kw",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "syncit-real-time-collaboration.firebaseapp.com",
   projectId: "syncit-real-time-collaboration",
   storageBucket: "syncit-real-time-collaboration.firebasestorage.app",
